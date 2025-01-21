ff1be233aade042c886bde43a07bea306f2da3a3
Parrycys <parrychan339@yahoo.com>
2025-01-22 02:00:40 +0800

    Renaming of chatbot, Greeting from chatbot and exit message

---
+++ src/main/java/Doobert.java
@@ -0,0 +1,10 @@
+public class Doobert {
+    public static void main(String[] arg) {
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm Doobert\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
